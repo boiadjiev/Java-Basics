7725e8dd84fc0b1f35eb72e4261198d9e8b8682f
boiadjiev <123622892+boiadjiev@users.noreply.github.com>
2023-02-14 12:51:08 +0200

    Create DepositCalculator.java

---
+++ 01. Simple Operations/Exercise/03. Deposit Calculator/DepositCalculator.java
@@ -0,0 +1,14 @@
+package FirstStepsInCoding.Exercise;
+
+import java.util.Scanner;
+
+public class DepositCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double deposit = Double.parseDouble(scanner.nextLine());
+        int month = Integer.parseInt(scanner.nextLine());
+        double percent = Double.parseDouble(scanner.nextLine());
+        double sum = deposit + month * ((deposit * percent / 100) / 12);
+        System.out.println(sum);
+    }
+}
