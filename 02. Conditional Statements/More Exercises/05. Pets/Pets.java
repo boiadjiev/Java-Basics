5f5ba5dc3b6c78de1e95fd5b5bd853801b145189
boiadjiev <123622892+boiadjiev@users.noreply.github.com>
2023-02-26 13:48:35 +0200

    Create Pets.java

---
+++ 02. Conditional Statements/More Exercises/05. Pets/Pets.java
@@ -0,0 +1,23 @@
+package ConditionalStatements.MoreExercises;
+
+import java.util.Scanner;
+
+public class Pets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int days = Integer.parseInt(scanner.nextLine());
+        int food = Integer.parseInt(scanner.nextLine());
+        double dogFood = Double.parseDouble(scanner.nextLine());
+        double catFood = Double.parseDouble(scanner.nextLine());
+        double turtleFood = Double.parseDouble(scanner.nextLine());
+
+        double foodNeeded = days * dogFood + days * catFood + days * turtleFood / 1000;
+        double diff = Math.abs(food - foodNeeded);
+
+        if (foodNeeded <= food) {
+            System.out.printf("%.0f kilos of food left.", Math.floor(diff));
+        } else {
+            System.out.printf("%.0f more kilos of food are needed.", Math.ceil(diff));
+        }
+    }
+}
