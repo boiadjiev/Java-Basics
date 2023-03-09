fa8168cf340951f9085efd097bbe7e7b293c9fa9
boiadjiev <123622892+boiadjiev@users.noreply.github.com>
2023-03-09 11:30:28 +0200

    Create NumbersFrom1to10.java

---
+++ 03. Conditional Statements Advanced/More Exercises/09. Numbers From 1 to 10/NumbersFrom1to10.java
@@ -0,0 +1,11 @@
+package ConditionalStatementsAdvanced.MoreExercises;
+
+public class NumbersFrom1to10 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 11) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
