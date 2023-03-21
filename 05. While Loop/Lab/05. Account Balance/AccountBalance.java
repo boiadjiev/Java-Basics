c9c798cd938ade64399fa146276d5f7882834881
boiadjiev <123622892+boiadjiev@users.noreply.github.com>
2023-03-21 12:26:29 +0200

    Create AccountBalance.java

---
+++ 05. While Loop/Lab/05. Account Balance/AccountBalance.java
@@ -0,0 +1,28 @@
+package WhileLoop.Lab;
+
+import java.util.Scanner;
+
+public class AccountBalance {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        double total = 0;
+
+        while (!"NoMoreMoney".equals(input)) {
+
+            double num = Double.parseDouble(input);
+
+            if (num < 0) {
+                System.out.println("Invalid operation!");
+                break;
+            }
+
+            System.out.printf("Increase: %.2f\n", num);
+            total += num;
+            input = scanner.nextLine();
+
+        }
+
+        System.out.printf("Total: %.2f", total);
+    }
+}
