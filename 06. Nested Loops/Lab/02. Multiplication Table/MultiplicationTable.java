12a0292d1fe6cd65c8c9f1866856c6932580daad
boiadjiev <123622892+boiadjiev@users.noreply.github.com>
2023-03-27 11:12:43 +0300

    Create MultiplicationTable.java

---
+++ 06. Nested Loops/Lab/02. Multiplication Table/MultiplicationTable.java
@@ -0,0 +1,14 @@
+package NestedLoops.Lab;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        for (int x = 1; x <= 10; x++) {
+
+            for (int y = 1; y <= 10; y++) {
+
+                System.out.printf("%d * %d = %d\n", x, y, x * y);
+            }
+        }
+    }
+}
