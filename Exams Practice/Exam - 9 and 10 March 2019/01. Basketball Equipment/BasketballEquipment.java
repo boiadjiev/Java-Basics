8e57537bbf8a3d799475cb1a193881aa952cba83
boiadjiev <123622892+boiadjiev@users.noreply.github.com>
2023-04-10 00:22:22 +0300

    Create BasketballEquipment.java

---
+++ Exams Practice/Exam - 9 and 10 March 2019/01. Basketball Equipment/BasketballEquipment.java
@@ -0,0 +1,16 @@
+package Exam9and10March2019;
+
+import java.util.Scanner;
+
+public class BasketballEquipment {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double annualFee = Double.parseDouble(scanner.nextLine());
+        double sneakers = annualFee - annualFee * 0.4;
+        double outfit = sneakers - sneakers * 0.2;
+        double ball = 1/4d * outfit;
+        double accessors = 1/5d * ball;
+        double expenses = annualFee + sneakers + outfit + ball + accessors;
+        System.out.printf("%.2f", expenses);
+    }
+}
