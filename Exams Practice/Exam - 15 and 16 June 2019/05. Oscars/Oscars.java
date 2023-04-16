6601ca2fe7b70be388d9b9cdfbd9526c10ecf212
boiadjiev <123622892+boiadjiev@users.noreply.github.com>
2023-04-16 16:27:33 +0300

    Create Oscars.java

---
+++ Exams Practice/Exam - 15 and 16 June 2019/05. Oscars/Oscars.java
@@ -0,0 +1,27 @@
+package Exam15and16June2019;
+
+import java.util.Scanner;
+
+public class Oscars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String actorName = scanner.nextLine();
+        double points = Double.parseDouble(scanner.nextLine());
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String assessorName = scanner.nextLine();
+            double assessorPoints = Double.parseDouble(scanner.nextLine());
+            points += assessorName.length() * assessorPoints / 2;
+
+            if (points > 1250.5) {
+                System.out.printf("Congratulations, %s got a nominee for leading role with %.1f!", actorName, points);
+                break;
+            }
+        }
+
+        if (points < 1250.5) {
+            System.out.printf("Sorry, %s you need %.1f more!", actorName, 1250.5 - points);
+        }
+    }
+}
