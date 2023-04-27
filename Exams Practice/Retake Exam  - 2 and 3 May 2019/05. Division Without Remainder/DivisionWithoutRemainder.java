2ebefb288c3cfdad967e8ef40ec13503233ffd0e
boiadjiev <123622892+boiadjiev@users.noreply.github.com>
2023-04-28 00:14:59 +0300

    Create DivisionWithoutRemainder.java

---
+++ Exams Practice/Retake Exam  - 2 and 3 May 2019/05. Division Without Remainder/DivisionWithoutRemainder.java
@@ -0,0 +1,31 @@
+package RetakeExam2and3May2019;
+
+import java.util.Scanner;
+
+public class DivisionWithoutRemainder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int p1 = 0;
+        int p2 = 0;
+        int p3 = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            int number = Integer.parseInt(scanner.nextLine());
+
+            if (number % 2 == 0) {
+                p1++;
+            }
+            if (number % 3 == 0) {
+                p2++;
+            }
+            if (number % 4 == 0) {
+                p3++;
+            }
+        }
+        System.out.printf("%.2f%%\n", p1 * 1.0 / n * 100);
+        System.out.printf("%.2f%%\n", p2 * 1.0 / n * 100);
+        System.out.printf("%.2f%%\n", p3 * 1.0 / n * 100);
+    }
+}
